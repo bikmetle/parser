12cb6a673e834947ba300cc9535fa1fe16f84c10
Marsel Gaifulin <fidmargai@gmail.com>
2024-10-14 06:17:07 +0300

    context-manager

--- main_netmonitor.py
+++ main_netmonitor.py
@@ -1,4 +1,6 @@
+from contextlib import contextmanager
 import json
+import logging
 import os
 import time
 from selenium import webdriver
@@ -8,74 +10,99 @@ from selenium.webdriver.firefox.service import Service
 from selenium.common.exceptions import JavascriptException
 from urls_to_skip import urls_to_skip
 
+logging.basicConfig(level=logging.INFO)
 
-def save_cookies(driver, path):
-    with open(path, 'w') as file:
-        json.dump(driver.get_cookies(), file)
 
-def load_cookies(driver, path):
-    with open(path, 'r') as file:
-        cookies = json.load(file)
+@contextmanager
+def selenium_driver():
+    options=Options()
+    firefox_profile = FirefoxProfile("/home/bikmetle/.mozilla/firefox/699rashk.default-release")
+    options.add_argument("--devtools")   
+    firefox_profile.set_preference("devtools.toolbox.selectedTool", "netmonitor")
+    firefox_profile.set_preference("devtools.netmonitor.persistlog", True)
+    options.profile = firefox_profile
+    geckodriver_path = "/usr/local/bin/geckodriver"
+    driver_service = Service(executable_path=geckodriver_path)
+
+    driver = webdriver.Firefox(
+        service=driver_service,
+        options=options,
+    )
+
+    try:
+        logging.info("Start new firefox session.")
+        yield driver
+    finally:
+        driver.quit()
+        logging.info("Stop the firefox session.")
+
+
+def saveCookies(driver):
+    cookies = driver.get_cookies()
+
+    with open('cookies.json', 'w') as file:
+        json.dump(cookies, file)
+    logging.info('New Cookies saved successfully')
+
+
+def loadCookies():
+    if 'cookies.json' in os.listdir():
+        with open('cookies.json', 'r') as file:
+            cookies = json.load(file)
         for cookie in cookies:
             driver.add_cookie(cookie)
+    else:
+        logging.info('No cookies file found')
+    
+    driver.refresh() # Refresh Browser after login
 
 
-options=Options()
-# firefox_profile = FirefoxProfile()
-firefox_profile = FirefoxProfile("/home/bikmetle/.mozilla/firefox/699rashk.default-release")
-options.add_argument("--devtools")   
-firefox_profile.set_preference("devtools.toolbox.selectedTool", "netmonitor")
-firefox_profile.set_preference("devtools.netmonitor.persistlog", True)
-options.profile = firefox_profile
-geckodriver_path = "/usr/local/bin/geckodriver"
-driver_service = Service(executable_path=geckodriver_path)
+def get_har_data(attempt=0):
+    if attempt > 10:
+        raise
 
-browser = webdriver.Firefox(
-    service=driver_service,
-    options=options,
-)
-load_cookies(browser, "cookies.txt")
+    attempt += 1
+    time.sleep(attempt*2)
 
-# record interesting stuff
-...
-
-project_name = input("Enter the project name: ")
-try:
-    project_dir = f"{project_name}_p"
-    os.mkdir(project_dir)
-except OSError as error:
-    print(f"Failed to create folder: {error}")
-
-browser.install_addon("har-export-trigger.zip", temporary=True)
-
-
-def get_har_data(seconds=1):
-    time.sleep(seconds)
     try:
-        har_data = browser.execute_async_script(
+        har_data = driver.execute_async_script(
             "HAR.triggerExport().then(arguments[0]);"
         )
     except JavascriptException:
-        seconds *= 2 
-        return get_har_data(seconds)
+        return get_har_data(attempt)
+
     return har_data
 
-har_data = get_har_data()
 
-save_cookies(browser, "cookies.txt")
-browser.quit()
+def save_har_data(har_data):
+    entry_count = 0
+    for entry in har_data['entries']:
+        entry_count += 1
 
+        if any(url in entry['request']['url'] for url in urls_to_skip):
+            continue
 
-entry_count = 0
-for entry in har_data['entries']:
-    entry_count += 1
+        file = f"{project_name}_p/{entry['startedDateTime']}.json"
+        with open(file, 'w', encoding='utf-8') as f:
+            json.dump(entry, f, ensure_ascii=False, indent=4)
 
-    if any(url in entry['request']['url'] for url in urls_to_skip):
-        continue
-
-    file = f"{project_name}_p/{entry['startedDateTime']}.json"
-    with open(file, 'w', encoding='utf-8') as f:
-        json.dump(entry, f, ensure_ascii=False, indent=4)
+    logging.info(f"{len(har_data['entries'])} requests saved")
 
 
-print(f"{len(har_data['entries'])} requests saved")
+with selenium_driver() as driver:
+    driver.get("https://lk.mango-office.ru")
+    loadCookies()
+
+    project_name = input("Enter the project name or `exit`: ")
+    saveCookies(driver)
+
+    if project_name != 'exit':
+        try:
+            project_dir = f"{project_name}_p"
+            os.mkdir(project_dir)
+        except OSError as error:
+            logging.info(f"Failed to create folder: {error}")
+
+        driver.install_addon("har-export-trigger.zip", temporary=True)
+        har_data = get_har_data()
+        save_har_data(har_data)
