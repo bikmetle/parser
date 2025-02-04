ae1c22a5c220b608d8b1b183365261eb48271d12
Marsel Gaifulin <fidmargai@gmail.com>
2025-02-04 17:15:35 +0300

    tunnel-flag

--- main_netmonitor.py
+++ main_netmonitor.py
@@ -1,22 +1,47 @@
 from contextlib import contextmanager
+from selenium import webdriver
+from selenium.common.exceptions import JavascriptException
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from urls_to_skip import urls_to_skip
 import json
 import logging
 import os
 import time
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-from selenium.webdriver.firefox.service import Service
-from selenium.common.exceptions import JavascriptException
-from urls_to_skip import urls_to_skip
+import subprocess
+from urllib.parse import urlparse
+
 
 logging.basicConfig(level=logging.INFO)
 
+root = os.path.dirname(__file__)
+
+url='https://hackerone.com/directory/programs?offers_bounties=true&order_direction=DESC&order_field=launched_at'
+hostname = urlparse(url).hostname.split('.')[0]
+cookies_file = f"cookies_{hostname}.json"
+is_tunnel_enabled = False
+
+
+def start_ssh_tunnel():
+    try:
+        command = ["ssh", "call", "-fN", "-D", "1080"]
+        subprocess.run(command, check=True)
+        logging.info("SSH command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        logging.info(f"An error occurred while executing the SSH command: {e}")
+    except Exception as e:
+        logging.info(f"Unexpected error: {e}")
 
 @contextmanager
 def selenium_driver():
     options=Options()
-    firefox_profile = FirefoxProfile("/home/bikmetle/.mozilla/firefox/699rashk.default-release")
+    
+    if is_tunnel_enabled:
+        firefox_profile = FirefoxProfile("/home/bikmetle/.mozilla/firefox/699rashk.default-release")
+    else:
+        firefox_profile = FirefoxProfile("/home/bikmetle/.mozilla/firefox/dlvoc3tb.default")
+
     options.add_argument("--devtools")   
     firefox_profile.set_preference("devtools.toolbox.selectedTool", "netmonitor")
     firefox_profile.set_preference("devtools.netmonitor.persistlog", True)
@@ -40,14 +65,14 @@ def selenium_driver():
 def saveCookies(driver):
     cookies = driver.get_cookies()
 
-    with open('cookies.json', 'w') as file:
+    with open(cookies_file, 'w') as file:
         json.dump(cookies, file)
     logging.info('New Cookies saved successfully')
 
 
 def loadCookies():
-    if 'cookies.json' in os.listdir():
-        with open('cookies.json', 'r') as file:
+    if cookies_file in os.listdir():
+        with open(cookies_file, 'r') as file:
             cookies = json.load(file)
         for cookie in cookies:
             driver.add_cookie(cookie)
@@ -90,7 +115,9 @@ def save_har_data(har_data):
 
 
 with selenium_driver() as driver:
-    driver.get("https://lk.mango-office.ru")
+    if is_tunnel_enabled:
+        start_ssh_tunnel()
+    driver.get(url)
     loadCookies()
 
     project_name = input("Enter the project name or `exit`: ")
